df72438214e2aea89dc24ce4c67fff4a01661641
Maheswaran Palaniselvan <maheswaran.palaniselvan@gmail.com>
2024-03-05 23:46:51 +0530

    downloader fix

--- renko_super/downloader.py
+++ renko_super/downloader.py
@@ -14,12 +14,28 @@ def get_symbols():
 
 
 def get_session(BASE) -> requests.Session:
-    _user_agent = requests.get(
-        "https://techfanetechnologies.github.io/latest-user-agent/user_agents.json"
-    ).json()[-2]
+    # _user_agent = requests.get(
+    #     "https://techfanetechnologies.github.io/latest-user-agent/user_agents.json"
+    # ).json()[-2]
     s = requests.Session()
     s.verify = True
-    s.headers.update({"User-Agent": _user_agent})
+    headers = {
+        "Host": "www.nseindia.com",
+        "Referer": "https://www.nseindia.com/get-quotes/equity?symbol=SBIN",
+        "X-Requested-With": "XMLHttpRequest",
+        "pragma": "no-cache",
+        "sec-fetch-dest": "empty",
+        "sec-fetch-mode": "cors",
+        "sec-fetch-site": "same-origin",
+        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.132 Safari/537.36",
+        "Accept": "*/*",
+        "Accept-Encoding": "gzip, deflate, br",
+        "Accept-Language": "en-GB,en-US;q=0.9,en;q=0.8",
+        "Cache-Control": "no-cache",
+        "Connection": "keep-alive",
+    }
+    s.headers.update(headers)
+    # s.headers.update({"User-Agent": _user_agent})
     s.get(BASE, timeout=(3.05, 10))
     return s
 
